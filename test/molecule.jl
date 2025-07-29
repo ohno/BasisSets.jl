649fad9a27e067e6b67ebc8adb90c1ea489d19e9
Shuhei Ohno <ohno.github@gmail.com>
2025-07-30 05:31:55 +0900

    Fix tests for molecule.jl

--- test/molecule.jl
+++ test/molecule.jl
@@ -1,23 +1,32 @@
 @time @testset "molecule.jl" begin
-    # benchmark for testing
+
     benchmark = Molecule(["H", "H"], [0.0 0.0 0.0; 0.0 0.0 0.74], [1, 1])
-    # test `molecule()`
+
+    println("test `molecule()`")
     mol1 = molecule("./data/hydrogen/h-atom.xyz")
-    # test `@molecule {}`
+    @test mol1 isa Molecule
+    @test mol1.atoms == benchmark.atoms
+    @test mol1.coords == benchmark.coords
+    @test mol1.numbers == benchmark.numbers
+
+    println("test `@molecule {}`")
     mol2 = @molecule {
-                H  0.0  0.0  0.0
-                H  0.0  0.0  0.74
-            }
-    # test `BasisSets.parse_xyz()`
+            H  0.0  0.0  0.0
+            H  0.0  0.0  0.74
+        }
+    @test mol2 isa Molecule
+    @test mol2.atoms == benchmark.atoms
+    @test mol2.coords == benchmark.coords
+    @test mol2.numbers == benchmark.numbers
+
+    println("test `BasisSets.parse_xyz()`")
     mol3 = BasisSets.parse_xyz("
             H  0.0  0.0  0.0
             H  0.0  0.0  0.74
         ")
-    # @test for all cases
-    for mol in [mol1, mol2, mol3]
-        @test mol isa Molecule
-        @test mol.atoms == benchmark.atoms
-        @test mol.coords == benchmark.coords
-        @test mol.numbers == benchmark.numbers
-    end
+    @test mol3 isa Molecule
+    @test mol3.atoms == benchmark.atoms
+    @test mol3.coords == benchmark.coords
+    @test mol3.numbers == benchmark.numbers
+
 end
